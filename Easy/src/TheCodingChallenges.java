3a5551e07df240879940554f82289c1c27cae839
Borislav Kostov <borislavkostov97@gmail.com>
2017-02-02 12:46:39 +0200

    The Coding Challenges

---
+++ Easy/src/TheCodingChallenges.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TheCodingChallenges {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int end = n-1;
+		System.out.println(n*end);
+	}
+}
